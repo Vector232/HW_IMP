0338ef291ade75e7fc6c8000c654d93743c5d597
Vlad Troyan <030705vt@mail.ru>
2023-07-26 11:43:01 +0300

    first red

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import datetime
+import application.db.people as people
+from application.salary import calculate_salary
+from dirty_main import *
 
 if __name__ == '__main__':
-    pass
+    print(people.get_employees())
+    print(calculate_salary())
+    print(func1())
+    print(datetime.datetime.now())
